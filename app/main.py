f7605b5aee883aca76fccfc2fc349c24c864d5ac
dmndpl <diamandopol.andrei10@gmail.com>
2024-10-07 16:31:15 +0100

    Status code for invalid api version

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import socket  # noqa: F401
 
 
-def create_api_version_response(correlation_id: int):
+def create_api_version_response(correlation_id: int, status_code=None):
     correlation_id_bytes = correlation_id.to_bytes(4, byteorder="big")
-    message_length = len(correlation_id_bytes).to_bytes(4, byteorder="big")
-    return message_length + correlation_id_bytes
+    message = correlation_id_bytes
+    if status_code:
+        message += status_code.to_bytes(2, byteorder="big")
+    message_length = len(message).to_bytes(4, byteorder="big")
+    return message_length + message
 
 def main():
     # You can use print statements as follows for debugging,
@@ -21,18 +24,18 @@ def main():
             print(f"Connected from {addr}")
             data = conn.recv(1024)
             print(data, 'EOF')
+            request_length = int.from_bytes(data[0:4], byteorder="big")
             data = data[4:]
             req_api_key_bytes = data[0:2]
             req_api_key = int.from_bytes(req_api_key_bytes, byteorder="big")
-            print(req_api_key)
             req_api_version_bytes = data[2:4]
             req_api_version = int.from_bytes(req_api_version_bytes, byteorder="big")
-            print(req_api_version)
             correlation_id_bytes = data[4:8]
             correlation_id = int.from_bytes(correlation_id_bytes, byteorder="big")
-            print(correlation_id)
-            
-            conn.sendall(create_api_version_response(correlation_id))
+            if req_api_version > 4 or req_api_version < 0:
+                conn.sendall(create_api_version_response(correlation_id, 35))
+            else:
+                conn.sendall(create_api_version_response(correlation_id))
 
 if __name__ == "__main__":
     main()
